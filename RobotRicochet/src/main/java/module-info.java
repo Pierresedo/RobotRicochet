71531689bf8a0a8a5ec027ec0419a033116c13fd
Pierresedo <99965943+Pierresedo@users.noreply.github.com>
2022-05-18 11:34:15 +0200

    Delete RobotRicochet directory

--- RobotRicochet/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.robotricochet {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.robotricochet to javafx.fxml;
-    exports com.example.robotricochet;
-}
